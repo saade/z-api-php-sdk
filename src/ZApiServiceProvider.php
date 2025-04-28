7a294619283ed20bb89ad4fb8ae6e9abeed6ba93
Saade <saade@outlook.com.br>
2025-04-28 16:11:12 -0300

    cleanup

--- src/ZApiServiceProvider.php
+++ src/ZApiServiceProvider.php
@@ -10,16 +10,8 @@ class ZApiServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
         $package
-            ->name('z-api-php-sdk')
-            ->hasConfigFile()
-            ->hasViews()
-            ->hasMigration('create_z_api_php_sdk_table')
-            ->hasCommand(ZApiCommand::class);
+            ->name('z-api')
+            ->hasConfigFile();
     }
 }
